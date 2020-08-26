ba860ae2f5ba77cee18a7cfc9572eee1b7e2ce76
nakhun9279 <fjfj9279@naver.com>
2020-08-26 22:26:53 +0900

    2.4.2 자바스크립트 표현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, {Fragment} from 'react';
-
-
+import React from 'react';
+ 
 function App() {
+  const name = '리액트';
   return (
-    <Fragment>
-    <h1>리액트 안녕!</h1>
-    <h2>잘 작동하니?</h2>
-    </Fragment>
+    <>
+      <h1>{name} 안녕!</h1>
+      <h2>잘 작동하니?</h2>
+    </>
   );
 }
+ 
 export default App;
