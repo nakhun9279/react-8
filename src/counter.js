a7d76c5ae5b1c8e3a0682923a7deea324de93230
nakhun9279 <fjfj9279@naver.com>
2020-08-27 04:46:27 +0900

    3.4.1-4 this.setstate에 객체 대신 함수인자 전달하기

--- src/counter.js
+++ src/counter.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
  
+
 class Counter extends Component {
   state = {
     number: 0,
@@ -16,6 +17,7 @@ class Counter extends Component {
           onClick={() => {
             // this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
             this.setState({ number: number + 1 });
+            this.setState({ number: this.state.number + 1 });
           }}
         >
           +1
