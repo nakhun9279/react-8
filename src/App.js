f9d6a7114a791815edf56aa94e81db555d6cd8ee
nakhun9279 <fjfj9279@naver.com>
2020-08-27 04:17:01 +0900

    3.3.6-1 name값을 숫자로 지정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import MyComponent from './MyComponent';
- 
+
+
 const App = () => {
-  return <MyComponent>리액트</MyComponent>;
+  return <MyComponent name={3}>리액트</MyComponent>;
 };
- 
+
+
+
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
- 
+import PropTypes from 'prop-types';
+
 const MyComponent = ({ name, children }) => {
   return (
     <div>
@@ -14,4 +15,9 @@ MyComponent.defaultProps = {
   name: '기본 이름'
 };
  
+
+MyComponent.propTypes = {
+    name: PropTypes.string
+  };
+  
 export default MyComponent;
