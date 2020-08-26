33b364366035d84be2cf99d4f181794b483776d0
nakhun9279 <fjfj9279@naver.com>
2020-08-27 04:34:16 +0900

    3.4.1-3 state를 constructor에서 꺼내기

--- src/counter.js
+++ src/counter.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
-
-
+ 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    // state의 초깃값 설정하기
-    this.state = {
-      number: 0,
-      fixedNumber: 0
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0
+  };
   render() {
     const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회합니다.
     return (
-      <div>
+        <div>
         <h1>{number}</h1>
         <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
@@ -29,7 +24,5 @@ class Counter extends Component {
     );
   }
 }
-
-
-
+ 
 export default Counter;
