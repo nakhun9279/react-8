4cf1e2123ad1deb89d90f06b0616d5d659bf0b2a
nakhun9279 <fjfj9279@naver.com>
2020-08-26 22:12:35 +0900

    2.4.1-3 Fragment활용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 
 function App() {
   return (
-    <div>
+    <Fragment>
     <h1>리액트 안녕!</h1>
     <h2>잘 작동하니?</h2>
-    </div>
+    </Fragment>
   );
 }
 export default App;
