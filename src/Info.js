6d328be549e88c342bed8825c39c359594ccabc3
nakhun9279 <fjfj9279@naver.com>
2020-08-27 21:12:50 +0900

    8.2.3-2 보이기/숨기기

--- src/Info.js
+++ src/Info.js
@@ -1,46 +1,21 @@
-import React, { useState, useEffect } from 'react';
-
-
-const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
-  useEffect(() => {
-    console.log('effect');
-    console.log(name);
-    return () => {
-      console.log('cleanup');
-      console.log(name);
-    };
-  });
-  const onChangeName = e => {
-    setName(e.target.value);
-  };
-
-
-
-const onChangeNickname = e => {
-    setNickname(e.target.value);
-  };
-
+import React, { useState } from 'react';
+import Info from './Info';
  
-  return (
-    <div>
-      <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
-      </div>
-      <div>
-        <div>
-          <b>이름:</b> {name}
-        </div>
-        <div>
-          <b>닉네임:</b> {nickname}
-        </div>
-      </div>
+const App = () => {
+const [visible, setVisible] = useState(false);
+return (
+  <div>
+      <button
+      onClick={() => {
+        setVisible(!visible);
+        }}
+      >
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr />
+    {visible && <Info />}
     </div>
-  );
+);
 };
-
-
-
-export default Info;
+ 
+export default App;
