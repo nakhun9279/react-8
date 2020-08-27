97d28f17060e4f363933fff3d8525c66a502128f
nakhun9279 <fjfj9279@naver.com>
2020-08-27 22:23:58 +0900

    8.3.2 인풋상태 관리하기

--- src/Info.js
+++ src/Info.js
@@ -2,17 +2,11 @@ import React, { useReducer } from 'react';
 
 
 function reducer(state, action) {
-// action.type에 따라 다른 작업 수행
-switch (action.type) {
-    case 'INCREMENT':
-      return { value: state.value + 1 };
-    case 'DECREMENT':
-      return { value: state.value - 1 };
-    default:
-      // 아무것도 해당되지 않을 때 기존 상태 반환
-      return state;
+    return {
+      ...state,
+      [action.name]: action.value
+    };
   }
-}
 
 
 
