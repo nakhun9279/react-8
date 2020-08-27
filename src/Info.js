365ef5f971152e7c472f0469f8993dc2840df4a6
nakhun9279 <fjfj9279@naver.com>
2020-08-27 22:09:42 +0900

    8.4 useMemo

--- src/Info.js
+++ src/Info.js
@@ -1,40 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer } from 'react';
+
+
+function reducer(state, action) {
+// action.type에 따라 다른 작업 수행
+switch (action.type) {
+    case 'INCREMENT':
+      return { value: state.value + 1 };
+    case 'DECREMENT':
+      return { value: state.value - 1 };
+    default:
+      // 아무것도 해당되지 않을 때 기존 상태 반환
+      return state;
+  }
+}
+
 
 
 const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
-  useEffect(() => { 
-    console.log('effect');
-    console.log(name);
-    return () => {
-      console.log('cleanup');
-      console.log(name);
-    };
+  const [state, dispatch] = useReducer(reducer, {
+    name: '',
+    nickname: ''
   });
-  const onChangeName = e => {
-    setName(e.target.value);
+  const { name, nickname } = state;
+  const onChange = e => {
+    dispatch(e.target);
   };
 
 
 
-const onChangeNickname = e => {
-    setNickname(e.target.value);
-  };
-
- 
-  return (
+return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
           <b>이름:</b> {name}
         </div>
         <div>
-          <b>닉네임:</b> {nickname}
+          <b>닉네임: </b>
+          {nickname}
         </div>
       </div>
     </div>
@@ -44,3 +50,4 @@ const onChangeNickname = e => {
 
 
 export default Info;
+
