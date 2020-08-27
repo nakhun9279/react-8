f20aeb94ec430a8f90bbbdb6065bb610eb56ca56
nakhun9279 <fjfj9279@naver.com>
2020-08-27 18:19:20 +0900

    5.3.2 컴포넌트 초기설정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import ValidationSample from './ValidationSample';
- 
+import ScrollBox from './ScrollBox';
+
+
 class App extends Component {
   render() {
     return (
-      <ValidationSample/>
+      <div>
+        <ScrollBox/>
+      </div>
     );
   }
 }
- 
+
+
+
 export default App;
+
